7993e93f9256ced164f5324389c49a511d9ca42c
shairhem <shairhem@gmail.com>
2016-02-09 22:34:44 +0800

    change interface to windows form

--- BatchRename/Program.cs
+++ BatchRename/Program.cs
@@ -4,118 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace BatchRename
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            rename();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new startForm());
             return;
         }
 
-        static void rename()
-        {
-            string choice = "";
-            string folder = "";
-            int x = 1;
-            string temp = "";
-            string ext = "";
-            int length = 0;
-            string op = "";
-            string filename = "";
-            char[] end = { '"' };
-            List<string> infofile = new List<string>();
-            StreamWriter writer;
-
-            Console.WriteLine("input directory: ");
-            folder = Console.ReadLine();
-            folder = folder.Trim(end);
-            Console.WriteLine("input filename: ");
-            filename = Console.ReadLine();
-            DirectoryInfo d = new DirectoryInfo(@folder);
-            FileInfo[] infos = d.GetFiles();
-            foreach (FileInfo f in infos)
-            {
-                Console.WriteLine("renaming " + f.Name);
-                if (f.Name != "infofile.txt")
-                {
-                    try
-                    {
-                        ext = f.Name.Substring(f.Name.LastIndexOf('.'));
-                        //length = f.Name.Length - 1;
-                        //Console.WriteLine(f.Name + " / " + f.Name.LastIndexOf('.') + " / " + ext);
-                        //Console.ReadKey();
-                        temp = folder + "\\" + filename + x + ext;
-                        //Console.WriteLine("renaming file" + x);
-                        //Console.WriteLine(f.Name.ToString());
-                        File.Move(f.FullName, temp);
-                        infofile.Add(temp);
-                        x++;
-                    }
-                    catch (Exception err)
-                    {
-                        ext = ".unknown";
-                        temp = folder + "\\" + filename + x + ext;
-                        File.Move(f.FullName, temp);
-                        infofile.Add(temp);
-                        x++;
-                    }
-                    
-                }
-            }
-            op = folder;
-            folder += "\\infofile.txt";
-            writer = new StreamWriter(@folder);
-            foreach (string i in infofile)
-            {
-                writer.WriteLine(i);
-            }
-            Console.Clear();
-            writer.Close();
-            Console.WriteLine("done! Continue? [Y/N]");
-            Console.WriteLine("fix Unknown?");
-            choice = Console.ReadLine();
-            choice = choice.ToUpper();
-            if (choice == "Y")
-            {
-                fixUnknown(d,op,filename);
-                rename();
-            }
-            Console.WriteLine("done! Continue? [Y/N]");
-            choice = Console.ReadLine();
-            choice = choice.ToUpper();
-            if (choice == "Y")
-            {
-                rename();
-            }
-            else return;
-            Console.ReadKey();
-        }
-
-        public static void fixUnknown(DirectoryInfo d,string folder,string filename)
-        {
-            FileInfo[] infos = d.GetFiles();
-            string ext = "";
-            string temp = "";
-            int x = 0;
-            foreach (FileInfo f in infos)
-            {
-                if (f.Name != "infofile.txt")
-                {
-                    ext = ".jpg";
-                    //length = f.Name.Length - 1;
-                    //Console.WriteLine(f.Name + " / " + f.Name.LastIndexOf('.') + " / " + ext);
-                    //Console.ReadKey();
-                    temp = folder + "\\" + filename + x  + ".unk" + ext;
-                    //Console.WriteLine("renaming file" + x);
-                    //Console.WriteLine(f.Name.ToString());
-                    Console.WriteLine(f.FullName + "  " + temp);
-                    File.Move(f.FullName, temp);
-                }
-                x++;
-            }
-        }
     }
 }
---
+++ BatchRename/startForm.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BatchRename
+{
+    public partial class startForm : Form
+    {
+        public startForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            textBox1.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(textBox1.Text) && !String.IsNullOrWhiteSpace(textBox2.Text))
+                rename();
+            else
+                MessageBox.Show("Must have folder path and filename");
+        }
+        
+        private void rename()
+        {
+            string filename = textBox2.Text;
+            string folder = textBox1.Text;
+            List<string> infofile = new List<string>();
+            DirectoryInfo d = new DirectoryInfo(@folder);
+            FileInfo[] infos = d.GetFiles();
+            int fileCount = infos.Count();
+            progressBar1.Maximum = fileCount;
+            string ext = "";
+            string temp;
+            int x = 0;
+            foreach (FileInfo f in infos)
+            {
+            //foreach start
+                temp = "";
+                label3.Text = "renaming " + f.Name;
+                label3.Update();
+                if(f.Name != "infofile.txt")
+                {
+                //if start
+                    try
+                    {
+                        ext = f.Name.Substring(f.Name.LastIndexOf('.'));
+                        temp = folder + "\\" + filename + x + ext;
+                        File.Move(f.FullName, temp);                       
+                    }
+                    catch (Exception err)
+                    {
+                        ext = ".jpg";
+                        temp = folder + "\\" + filename + x + ext;
+                        File.Move(f.FullName, temp);
+                    }
+                    infofile.Add(temp);
+                    x++;
+
+                }
+                //end of if
+                progressBar1.Increment(1);
+            }
+            //end of foreach
+            writeToInfofile(filename,folder,infofile);
+            MessageBox.Show("done");
+        }
+
+        private void writeToInfofile(string filename, string folder, List<string> infofile)
+        {
+            folder += "\\infofile.txt";
+            StreamWriter writer = new StreamWriter(@folder);
+            foreach (string i in infofile)
+            {
+                writer.WriteLine(i);
+            }
+            writer.Close();
+        }
+    }
+}
+
